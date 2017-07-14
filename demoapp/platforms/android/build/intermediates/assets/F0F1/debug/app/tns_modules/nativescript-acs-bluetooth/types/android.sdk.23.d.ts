e301c903108532415f6bdfc25cbe2f5fcd7277e7
luke <lukeluluke@gmail.com>
2017-07-14 14:15:22 +1000

    delete node module

--- demoapp/platforms/android/build/intermediates/assets/F0F1/debug/app/tns_modules/nativescript-acs-bluetooth/types/android.sdk.23.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./_helpers.d.ts" />
-/// <reference path="./android.sdk.23/java.d.ts" />
-/// <reference path="./android.sdk.23/android.d.ts" />
-/// <reference path="./android.sdk.23/android.bluetooth.d.ts" />
-/// <reference path="./android.sdk.23/android.content.d.ts" />
