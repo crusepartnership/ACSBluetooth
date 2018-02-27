2506b5076d9b3186dfaa36e894cc950c8e99bca9
luke <luke0427@hotmail.com>
2018-02-27 15:21:03 +1100

    add function reference to remove error mesage when debug app  [INTEGRALCS-6208] (#4)
    * add function reference to remove error mesage when debug app  [INTEGRALCS-6208]
    
    * remove .idea folder [INTEGRALCS-6208]

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,18 @@
 import { Common } from './acs-bluetooth.common';
+import {Observable} from "rxjs/Observable";
 export declare class ACSBluetooth extends Common {
-  // define your typings manually
-  // or..
-  // take the ios or android .d.ts files and copy/paste them here
+    // define your typings manually
+    // or..
+    // take the ios or android .d.ts files and copy/paste them here
+
+    public startScanningForDevices() : void;
+    public stopScanningForDevices() : void;
+    public isPermissionGranted() : boolean;
+    public disconnect(): void;
+    public connect(detail: any): boolean;
+    public requestCoarseLocationPermission(): boolean;
+    public isScanning(): Observable<boolean>;
+    public getCardUid(): Observable<string>;
+    public isReaderConnected(): Observable<boolean>;
+    public scanResultsObservable(): Observable<any>;
 }
